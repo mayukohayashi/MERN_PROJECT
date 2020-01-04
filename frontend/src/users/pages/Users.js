e9ef509a1703d7c3ab74be93ef37331b28fbd43f
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2020-01-04 17:53:54 +0900

    add: set usersList userItem imported to userpage

--- frontend/src/users/pages/Users.js
+++ frontend/src/users/pages/Users.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
+import UsersList from '../components/UsersList/UsersList';
+
 const Users = () => {
-  return <h2>Users page</h2>;
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'Mayuko Hayashi',
+      image: 'https://image.flaticon.com/icons/png/512/64/64572.png',
+      places: 3
+    }
+  ];
+
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
