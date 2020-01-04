8eb98bd292284e6c776763c620f1a834c0a2a459
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2020-01-04 18:23:22 +0900

    add: set Avatar component in UserItem

--- frontend/src/users/components/UserItem/UserItem.js
+++ frontend/src/users/components/UserItem/UserItem.js
@@ -2,17 +2,19 @@ import React from 'react';
 
 import './UserItem.css';
 
+import Avatar from '../../../shared/components/UIElements/Avatar/Avatar';
+
 const UserItem = props => {
   return (
     <li className="user-item">
       <div className="user-item__content">
         <div className="user-item__image">
-          <img src={props.image} alt={props.name} />
+          <Avatar image={props.image} alt={props.name} />
         </div>
         <div className="user-item__info">
           <h2>{props.name}</h2>
           <h3>
-            {props.placeCount} {props.praceCount === 1 ? 'Place' : 'Places'}
+            {props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places'}
           </h3>
         </div>
       </div>
