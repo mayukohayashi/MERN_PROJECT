503248b92aa1b90102c595d70f54a7fa489f6b70
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2020-01-05 16:54:20 +0900

    feat: set NavLinks for MainNavigation

--- frontend/src/shared/components/Navigation/MainNavigation/MainNavigation.js
+++ frontend/src/shared/components/Navigation/MainNavigation/MainNavigation.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import './MainNavigation.css';
 
 import MainHeader from '../MainHeader/MainHeader';
+import NavLinks from '../NavLinks/NavLinks';
 
 const MainNavigation = props => {
   return (
@@ -16,7 +17,9 @@ const MainNavigation = props => {
       <h1 className="main-navigation__title">
         <Link to="/">YourPlaces</Link>
       </h1>
-      <nav>...</nav>
+      <nav>
+        <NavLinks />
+      </nav>
     </MainHeader>
   );
 };
--- frontend/src/shared/components/Navigation/NavLinks/NavLinks.js
+++ frontend/src/shared/components/Navigation/NavLinks/NavLinks.js
@@ -1,3 +1,27 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import './NavLinks.css';
+
+const NavLinks = props => {
+  return (
+    <ul className="nav-link">
+      <li>
+        <NavLink to="/" exact>
+          All Users
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/u1/places">My Places</NavLink>
+      </li>
+      <li>
+        <NavLink to="/places/new">Add Place</NavLink>
+      </li>
+      <li>
+        <NavLink to="/auth">Authenticate</NavLink>
+      </li>
+    </ul>
+  );
+};
+
+export default NavLinks;
