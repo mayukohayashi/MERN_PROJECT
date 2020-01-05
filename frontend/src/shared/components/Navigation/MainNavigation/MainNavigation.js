c0068d34ac2ee4d8581641042a55e0482d5e3b2a
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2020-01-05 17:43:07 +0900

    fix: use react.fragment

--- frontend/src/shared/components/Navigation/MainNavigation/MainNavigation.js
+++ frontend/src/shared/components/Navigation/MainNavigation/MainNavigation.js
@@ -9,24 +9,26 @@ import SideDrawer from '../SideDrawer/SideDrawer';
 
 const MainNavigation = props => {
   return (
-    <SideDrawer>
-      <nav className="main-navigation__drawer-nav">
-        <NavLinks />
-      </nav>
-    </SideDrawer>
-    <MainHeader>
-      <button className="main-navigation__menu-btn">
-        <span />
-        <span />
-        <span />
-      </button>
-      <h1 className="main-navigation__title">
-        <Link to="/">YourPlaces</Link>
-      </h1>
-      <nav className="main-navigation__header-nav">
-        <NavLinks />
-      </nav>
-    </MainHeader>
+    <React.Fragment>
+      <SideDrawer>
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+      <MainHeader>
+        <button className="main-navigation__menu-btn">
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/">YourPlaces</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </React.Fragment>
   );
 };
 
