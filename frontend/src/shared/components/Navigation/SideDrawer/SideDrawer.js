5186f9fb93f68936e00e61e4f7d5bc0dc2dce8bc
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2020-01-05 18:35:07 +0900

    feat: add animation side drawer open and close for mobile

--- frontend/src/shared/components/Navigation/SideDrawer/SideDrawer.js
+++ frontend/src/shared/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
+import { CSSTransition } from 'react-transition-group';
 
 import './SideDrawer.css';
 
 const SideDrawer = props => {
-  const content = <aside className="side-drawer">{props.children}</aside>;
+  const content = (
+    <CSSTransition
+      in={props.show}
+      timeout={200}
+      classNames="slide-in-left"
+      mountOnEnter
+      unmountOnExit
+    >
+      <aside className="side-drawer" onClick={props.onClick}>{props.children}</aside>
+    </CSSTransition>
+  );
 
-  return ReactDom.createPortal(content, document.getElementById('drawer-hook'));
+  return ReactDOM.createPortal(content, document.getElementById('drawer-hook'));
 };
 
 export default SideDrawer;
