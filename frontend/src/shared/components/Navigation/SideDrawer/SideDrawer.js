429d4168415e12a27451ccddf78991c9b268aec5
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2020-01-05 20:05:15 +0900

    feat: when click drawer's link, drawer will disapper

--- frontend/src/shared/components/Navigation/SideDrawer/SideDrawer.js
+++ frontend/src/shared/components/Navigation/SideDrawer/SideDrawer.js
@@ -13,7 +13,9 @@ const SideDrawer = props => {
       mountOnEnter
       unmountOnExit
     >
-      <aside className="side-drawer" onClick={props.onClick}>{props.children}</aside>
+      <aside className="side-drawer" onClick={props.onClick}>
+        {props.children}
+      </aside>
     </CSSTransition>
   );
 
