398e6bd6637b1c41f1cb7f57a02a475751107250
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2020-01-06 21:13:07 +0900

    done: set basic format component in newplace

--- frontend/src/places/pages/NewPlace/NewPlace.js
+++ frontend/src/places/pages/NewPlace/NewPlace.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-import Input from '../../shared/components/FormElements/Input';
+import './NewPlace.css';
+
+import Input from '../../../shared/components/FormElements/Input/Input';
 
 const NewPlace = () => {
   return (
     <form className="place-form">
-      <Input type="text" label="Title" />
+      <Input element="input" type="text" label="Title" />
     </form>
   );
 };
