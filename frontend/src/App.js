125283c550f5dc204841b35129c7e6d7a062562c
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2020-01-06 21:09:16 +0900

    add: set and into file page/NewPlace component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ import {
 } from 'react-router-dom';
 
 import Users from './users/pages/Users';
-import NewPlace from './places/pages/NewPlace';
+import NewPlace from './places/pages/NewPlace/NewPlace';
 import UserPlaces from './places/pages/UserPlaces';
 
 import MainNavigation from './shared/components/Navigation/MainNavigation/MainNavigation';
