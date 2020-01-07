7f1e7ff16f8673e091d2b181e370b2adddfb2e17
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2020-01-07 15:29:06 +0900

    feat: share input values and adding multiple inputs

--- frontend/src/places/pages/NewPlace/NewPlace.js
+++ frontend/src/places/pages/NewPlace/NewPlace.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
 import './NewPlace.css';
 
 import Input from '../../../shared/components/FormElements/Input/Input';
-import { VALIDATOR_REQUIRE } from '../../../shared/components/util/validators';
+import {
+  VALIDATOR_REQUIRE,
+  VALIDATOR_MINLENGTH
+} from '../../../shared/components/util/validators';
 
 const NewPlace = () => {
+  const titleInputHandler = useCallback((id, value, isValid) => {}, []);
+
+  const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
+
   return (
     <form className="place-form">
       <Input
+        id="title"
         element="input"
         type="text"
         label="Title"
         validators={[VALIDATOR_REQUIRE()]}
         errorText="Please enter a valid title"
+        onInput={titleInputHandler}
+      />
+
+      <Input
+        id="description"
+        element="textarea"
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description at least 5 character"
+        onInput={descriptionInputHandler}
       />
     </form>
   );
--- frontend/src/shared/components/FormElements/Input/Input.js
+++ frontend/src/shared/components/FormElements/Input/Input.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useEffect } from 'react';
 
 import './Input.css';
 
@@ -28,6 +28,13 @@ const Input = props => {
     value: '',
     isValid: false
   });
+
+  const { id, onInput } = props;
+  const { value, isValid } = inputState;
+
+  useEffect(() => {
+    onInput(id, value, isValid);
+  }, [id, value, isValid, onInput]);
 
   const changeHandler = event => {
     dispatch({
