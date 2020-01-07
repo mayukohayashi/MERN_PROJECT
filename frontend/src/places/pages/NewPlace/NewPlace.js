f9de7d5469e810fe8b3f9843e14b8864ba13793c
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2020-01-07 17:02:52 +0900

    done: form component for new place

--- frontend/src/places/pages/NewPlace/NewPlace.js
+++ frontend/src/places/pages/NewPlace/NewPlace.js
@@ -57,8 +57,13 @@ const NewPlace = () => {
     });
   }, []);
 
+  const placeSubmitHandler = event => {
+    event.preventDefault();
+    console.log(formState.inputs); //send this to the backend(MongoDB)
+  };
+
   return (
-    <form className="place-form">
+    <form className="place-form" onSubmit={placeSubmitHandler}>
       <Input
         id="title"
         element="input"
@@ -74,9 +79,19 @@ const NewPlace = () => {
         element="textarea"
         label="Description"
         validators={[VALIDATOR_MINLENGTH(5)]}
-        errorText="Please enter a valid description at least 5 character"
+        errorText="Please enter a valid description (at least 5 character)"
         onInput={inputHandler}
       />
+
+      <Input
+        id="address"
+        element="input"
+        label="Address"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid address"
+        onInput={inputHandler}
+      />
+
       <Button type="submit" disabled={!formState.isValid}>
         ADD PLACE
       </Button>
