3c7bf84871a79569a17b18037b615e414a844f44
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2020-01-09 16:58:55 +0900

    add: auth.loggedin to header navlinks

--- frontend/src/shared/components/Navigation/NavLinks/NavLinks.js
+++ frontend/src/shared/components/Navigation/NavLinks/NavLinks.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 
 import './NavLinks.css';
 
+import { AuthContext } from '../../../contexts/auth-context';
+
 const NavLinks = props => {
+  const auth = useContext(AuthContext);
+
   return (
     <ul className="nav-links">
       <li>
@@ -11,15 +15,21 @@ const NavLinks = props => {
           All Users
         </NavLink>
       </li>
-      <li>
-        <NavLink to="/u1/places">My Places</NavLink>
-      </li>
-      <li>
-        <NavLink to="/places/new">Add Place</NavLink>
-      </li>
-      <li>
-        <NavLink to="/auth">Authenticate</NavLink>
-      </li>
+      {auth.isLoggedIn && (
+        <li>
+          <NavLink to="/u1/places">My Places</NavLink>
+        </li>
+      )}
+      {auth.isLoggedIn && (
+        <li>
+          <NavLink to="/places/new">Add Place</NavLink>
+        </li>
+      )}
+      {!auth.isLoggedIn && (
+        <li>
+          <NavLink to="/auth">Authenticate</NavLink>
+        </li>
+      )}
     </ul>
   );
 };
