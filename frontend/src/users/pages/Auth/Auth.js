c374700bb8dccc5ff42d14d4aba75ea898dc8a3c
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2020-01-09 15:11:25 +0900

    done: create auth page with login auth

--- frontend/src/users/pages/Auth/Auth.js
+++ frontend/src/users/pages/Auth/Auth.js
@@ -2,29 +2,65 @@ import React from 'react';
 
 import './Auth.css';
 
-import { useForm } from '../../shared/hooks/form-hook';
+import { useForm } from '../../../shared/hooks/form-hook';
 
-import Input from '../../../shared/components/FormElements/Input/Input'
+import Card from '../../../shared/components/UIElements/Card/Card';
+import Input from '../../../shared/components/FormElements/Input/Input';
 import Button from '../../../shared/components/FormElements/Button/Button';
 import {
-  VALIDATOR_REQUIRE,
+  VALIDATOR_EMAIL,
   VALIDATOR_MINLENGTH
 } from '../../../shared/components/util/validators';
 
 const Auth = () => {
+  const [formState, inputHandler] = useForm(
+    {
+      email: {
+        value: '',
+        isValid: false
+      },
+      password: {
+        value: '',
+        isValid: false
+      }
+    },
+    false
+  );
+
+  const authSubmitHandler = event => {
+    event.preventDefault();
+    console.log(formState.inputs);
+  };
 
   return (
-  <h1 className="authentication__header">Login? Sign In?</h1>
-  <form>
-
-  <Input />
-    <Input />
-    <Button type="submit">
-      LogIn
-    </Button>
-
-  </form>
-  )
+    <Card className="authentication">
+      <h2>Log In</h2>
+      <hr />
+      <form onSubmit={authSubmitHandler}>
+        <Input
+          id="email"
+          element="input"
+          type="email"
+          label="Email"
+          validators={[VALIDATOR_EMAIL()]}
+          errorText="Please enter a valid email address"
+          onInput={inputHandler}
+        />
+        <Input
+          id="password"
+          element="input"
+          type="password"
+          label="Password"
+          validators={[VALIDATOR_MINLENGTH(5)]}
+          errorText="Please enter a valid password. (at least 5 characters)"
+          onInput={inputHandler}
+        />
+        <Button type="submit" disabled={!formState.isValid}>
+          LOGIN
+        </Button>
+      </form>
+    </Card>
+  );
 };
 
 export default Auth;
