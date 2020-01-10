277a692551a10656a286234ec7c488e38664bb8e
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2020-01-10 18:31:18 +0900

    feat: set error model

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const HttpError = require('../models/http-error');
+
 const router = express.Router();
 
 const DUMMY_PLACES = [
@@ -24,9 +26,7 @@ router.get('/:pid', (req, res, next) => {
   });
 
   if (!place) {
-    const error = new Error('Could not find a place for the provided ID');
-    error.code = 404;
-    throw error;
+    throw new HttpError('Could not find a place for the provided ID❗', 404);
   }
 
   res.json({ place }); // => { place } => { place: place }
@@ -40,9 +40,9 @@ router.get('/user/:uid', (req, res, next) => {
   });
 
   if (!place) {
-    const error = new Error('Could not find a place for the provided user ID');
-    error.code = 404;
-    return next(error);
+    return next(
+      new HttpError('Could not find a place for the provided user ID❗', 404)
+    );
   }
 
   res.json({ place });
