0bfb9608ab2639706b47b074969462c045757290
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2020-01-10 20:18:58 +0900

    feat: create Patch updatePlace /api/places/:pid

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -69,7 +69,29 @@ const createPlace = (req, res, next) => {
   res.status(201).json({ place: createdPlace });
 };
 
+// PATCH /api/places/:pid
+const updatePlace = (req, res, next) => {
+  const { title, description } = req.body;
+  const placeId = req.params.pid;
+
+  const updatedPlace = { ...DUMMY_PLACES.find(p => p.id === placeId) };
+  const placeIndex = DUMMY_PLACES.findIndex(p => p.id === placeId);
+  updatedPlace.title = title;
+  updatedPlace.description = description;
+
+  DUMMY_PLACES[placeIndex] = updatedPlace;
+
+  res.status(200).json({ place: updatedPlace });
+};
+
+// DELETE /api/places/:pid
+// const deletePlace = (req, res, next) => {
+//   const
+// }
+
 // export multiple functions
 exports.getPlaceById = getPlaceById;
 exports.getPlaceByUserId = getPlaceByUserId;
 exports.createPlace = createPlace;
+exports.updatePlace = updatePlace;
+// exports.deletePlace = deletePlace;
