39eb9eac3ce27ba49955d598fcf4fe9bbb33d620
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2020-01-10 20:39:09 +0900

    fix: GET placesByUserId not one place but places

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -34,20 +34,20 @@ const getPlaceById = (req, res, next) => {
 };
 
 // GET /api/places/users/:uid
-const getPlaceByUserId = (req, res, next) => {
+const getPlacesByUserId = (req, res, next) => {
   const userId = req.params.uid;
 
-  const place = DUMMY_PLACES.find(p => {
+  const places = DUMMY_PLACES.filter(p => {
     return p.creator === userId;
   });
 
-  if (!place) {
+  if (!places || places.length === 0) {
     return next(
-      new HttpError('Could not find a place for the provided user ID❗', 404)
+      new HttpError('Could not find places for the provided user ID❗', 404)
     );
   }
 
-  res.json({ place });
+  res.json({ places });
 };
 
 // POST /api/places
@@ -93,7 +93,7 @@ const deletePlace = (req, res, next) => {
 
 // export multiple functions
 exports.getPlaceById = getPlaceById;
-exports.getPlaceByUserId = getPlaceByUserId;
+exports.getPlacesByUserId = getPlacesByUserId;
 exports.createPlace = createPlace;
 exports.updatePlace = updatePlace;
 exports.deletePlace = deletePlace;
