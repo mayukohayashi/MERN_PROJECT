d0d832d21db115a3f6ed612c05d041372fe20dfb
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2020-01-10 16:35:40 +0900

    check: get routes to appear

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -1,3 +1,10 @@
 const express = require('express');
 
 const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  console.log('GET Request in Places');
+  res.json({message: 'it works🙏'});
+})
+
+module.exports = router;
