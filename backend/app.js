2629cc460f3fc316024fe97d343568d081303c32
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2020-01-10 19:55:00 +0900

    feat: handling error for unsupported routes

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,19 @@ const bodyParser = require('body-parser');
 
 const placesRoutes = require('./routes/places-routes');
 
+const HttpError = require('./models/http-error');
+
 const app = express();
 
 app.use(bodyParser.json());
 
 // => /api/places/...
 app.use('/api/places', placesRoutes);
+
+app.use((req, res, next) => {
+  const error = new HttpError('Could not find this routes', 404);
+  throw error;
+});
 
 app.use((error, req, res, next) => {
   if (res.headerSent) {
