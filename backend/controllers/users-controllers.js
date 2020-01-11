439ca9d494b6121992300929588d8586f1b60926
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2020-01-11 17:57:07 +0900

    add: validation for POST signup

--- backend/controllers/users-controllers.js
+++ backend/controllers/users-controllers.js
@@ -1,4 +1,5 @@
 const uuid = require('uuid/v4');
+const { validationResult } = require('express-validator');
 
 const HttpError = require('../models/http-error');
 
@@ -26,6 +27,13 @@ const getUsers = (req, res, next) => {
 };
 
 const signUp = (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    console.log(errors);
+    throw new HttpError('Invalid inputs passed, please check your data', 422);
+  }
+
   const { name, email, password } = req.body;
 
   const hasUser = DUMMY_USERS.find(u => u.email === email);
--- backend/routes/users-routes.js
+++ backend/routes/users-routes.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const { check } = require('express-validator');
 
 const usersControllers = require('../controllers/users-controllers');
 
 const router = express.Router();
 
 router.get('/', usersControllers.getUsers);
-router.post('/signup', usersControllers.signUp);
+
+router.post(
+  '/signup',
+  [
+    check('name')
+      .not()
+      .isEmpty(),
+    check('email')
+      .normalizeEmail() // TEST@test.com => test@test.com
+      .isEmail(),
+    check('password').isLength({ min: 6 })
+  ],
+  usersControllers.signUp
+);
 router.post('/login', usersControllers.logIn);
 
 module.exports = router;
