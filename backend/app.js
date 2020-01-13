5c94953a39b77cb08cb18f8255280bd38823661d
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2020-01-13 16:46:17 +0900

    feat: setHeader in backend for CORS

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,17 @@ const app = express();
 
 app.use(bodyParser.json());
 
+// for CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  next();
+});
+
 app.use('/api/users', usersRoutes);
 app.use('/api/places', placesRoutes); // => /api/places/...
 
