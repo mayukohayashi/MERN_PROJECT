2527761fafc64238b362081d42058c6476e7af9d
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2020-01-13 20:29:07 +0900

    refac: refactoring page GET users req using custom html-hook

--- frontend/src/users/pages/Users.js
+++ frontend/src/users/pages/Users.js
@@ -1,47 +1,33 @@
 import React, { useEffect, useState } from 'react';
 
-import UsersList from '../components/UsersList/UsersList';
+import { useHttpClient } from '../../shared/hooks/http-hook';
 
+import UsersList from '../components/UsersList/UsersList';
 import ErrorModal from '../../shared/components/UIElements/ErrorModal';
 import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner/LoadingSpinner';
 
 const Users = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState();
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+
   const [loadedUsers, setLoadedUsers] = useState();
 
   useEffect(() => {
-    const sendRequest = async () => {
-      setIsLoading(true);
-
+    const fetchUsers = async () => {
       try {
-        const response = await fetch('http://localhost:6200/api/users');
-
-        const responseData = await response.json();
-
-        if (!response.ok) {
-          throw new Error(responseData.message);
-        }
+        const responseData = await sendRequest(
+          'http://localhost:6200/api/users'
+        );
 
         setLoadedUsers(responseData.users);
-        setIsLoading(false);
-      } catch (err) {
-        setError(err.message);
-      }
-
-      setIsLoading(false);
+      } catch (err) {}
     };
 
-    sendRequest();
-  }, []);
-
-  const errorHandler = () => {
-    setError(null);
-  };
+    fetchUsers();
+  }, [sendRequest]);
 
   return (
     <React.Fragment>
-      <ErrorModal error={error} onClear={errorHandler} />
+      <ErrorModal error={error} onClear={clearError} />
       {isLoading && (
         <div className="center">
           <LoadingSpinner />
