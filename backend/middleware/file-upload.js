22d131559068bf3cc19b890b04efbea0d6445ac1
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2020-01-14 20:43:12 +0900

    feat: only image(jpeg,jpg,png) to upload

--- backend/middleware/file-upload.js
+++ backend/middleware/file-upload.js
@@ -12,7 +12,7 @@ const fileUpload = multer({
 
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, 'uploads/images')
+      cb(null, 'uploads/images');
     },
     filename: (req, file, cb) => {
       const ext = MIME_TYPE_MAP[file.mimetype];
@@ -21,7 +21,12 @@ const fileUpload = multer({
     }
   }),
 
-  fileFilter:
+  fileFilter: (req, file, cb) => {
+    const isValid = !!MIME_TYPE_MAP[file.mimetype];
+    const error = isValid ? null : new Error('Invalid mime type!');
+
+    cb(error, isValid);
+  }
 });
 
 module.exports = fileUpload;
