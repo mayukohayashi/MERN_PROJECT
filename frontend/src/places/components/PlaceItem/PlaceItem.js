9faf54aef53162daaf57117b2a841d45842178cf
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2020-01-15 19:30:50 +0900

    feat: deleting and uploading place with auth(frontend)

--- frontend/src/places/components/PlaceItem/PlaceItem.js
+++ frontend/src/places/components/PlaceItem/PlaceItem.js
@@ -35,7 +35,11 @@ const PlaceItem = props => {
     try {
       await sendRequest(
         `http://localhost:6200/api/places/${props.id}`,
-        'DELETE'
+        'DELETE',
+        null,
+        {
+          Authorization: `Bearer ${auth.token}`
+        }
       );
 
       props.onDelete(props.id);
