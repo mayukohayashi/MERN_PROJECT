55873f1fcca76c3cca7eee91ee751a507c1a8c8e
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2020-01-16 19:03:38 +0900

    feat: add authentication edit on backend

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -160,6 +160,15 @@ const updatePlace = async (req, res, next) => {
     return next(error);
   }
 
+  if (place.creator.toString() !== req.userData.userId) {
+    const error = new HttpError(
+      'You are not allowed to edit this place❗',
+      401
+    );
+
+    return next(error);
+  }
+
   place.title = title;
   place.description = description;
 
