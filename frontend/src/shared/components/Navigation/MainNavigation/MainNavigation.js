3b59b4b3e2420a8ec81463add9875c1d179c8907
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2020-01-18 19:42:39 +0900

    fix: some styling

--- frontend/src/shared/components/Navigation/MainNavigation/MainNavigation.js
+++ frontend/src/shared/components/Navigation/MainNavigation/MainNavigation.js
@@ -38,7 +38,7 @@ const MainNavigation = props => {
           <span />
         </button>
         <h1 className="main-navigation__title">
-          <Link to="/">YourPlaces</Link>
+          <Link to="/">DAYZ</Link>
         </h1>
         <nav className="main-navigation__header-nav">
           <NavLinks />
