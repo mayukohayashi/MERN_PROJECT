96b6a95071a7be48827890be957693c275d0c3f7
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2020-01-23 20:51:25 +0900

    test: enyzyme and adapter16, Card component

---
+++ frontend/src/shared/components/UIElements/Card/Card.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Card from './Card';
+
+it('should render card component', () => {
+  expect(shallow(<Card />)).toMatchSnapshot();
+});
